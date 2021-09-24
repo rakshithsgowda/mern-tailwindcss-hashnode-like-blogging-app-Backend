94db3fb351e8cde0446a30fea7d82ef0976f8fdf
rakshith <rakshit.s.gowda@gmail.com>
2021-09-24 09:40:28 +0530

    prevent  user registration for already existing user

--- controllers/users/usersController.js
+++ controllers/users/usersController.js
@@ -4,6 +4,10 @@ const User = require('../../model/user/User')
 // Register
 // -------------------------------
 const userRegisterController = async (req, res) => {
+  // check if user EXISTS already
+  const userExist = await User.findOne({ email: req?.body?.email })
+  if (userExist) throw new Error('User already exists')
+
   // console.log(req?.body)
   try {
     // Register User
